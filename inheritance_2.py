39ab75c3c4e59a03e25beb741eeb2d5353c55d59
ilya-baykov <i-baykov@internet.ru>
2023-02-01 14:40:03 +0300

    Добавил класс SweetTooth

--- inheritance_2.py
+++ inheritance_2.py
@@ -1,4 +1,5 @@
 class Initialization:
+
     def __init__(self, capacity: int, food: list[str]):
         if isinstance(capacity, int):
             self.capacity = capacity
@@ -22,3 +23,28 @@ class MeatEater(Initialization):
 
     def __str__(self):
         return f"{self.capacity} мясоедов в Москве! Помимо мясо они едят еще и {self.food}"
+
+
+class SweetTooth(Initialization):
+    def __init__(self, capacity: int, food: list[str]):
+        super().__init__(capacity, food)
+
+    def __str__(self):
+        return f"Сладкоежик в Москве {self.capacity}. Их самая любимая еда {self.food}"
+
+    def __eq__(self, other: (int, MeatEater, Vegetarian)):
+        if isinstance(other, int):
+            return other == self.capacity
+        elif issubclass(other, Initialization):
+            return self.capacity == other.capacity
+        else:
+            return f"Невозможно сравнить количество сладкоежек с {other}"
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.capacity < other
+        elif issubclass(other, Initialization):
+            return self.capacity < other.capacity
+        else:
+            return f"Невозможно сравнить количество сладкоежек в {other}"
+
