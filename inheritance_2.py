a7edb075585b1ae15caf3cbd902abe5b9f8c2027
ilya-baykov <i-baykov@internet.ru>
2023-02-01 14:56:08 +0300

    Исправил ошибку в магических методах сравнения

--- inheritance_2.py
+++ inheritance_2.py
@@ -13,8 +13,8 @@ class Vegetarian(Initialization):
         super().__init__(capacity, food)
 
     def __str__(self):
-        return f"{self.capacity} людей предпочитают не есть мясо!" \
-               f"Они предпочитают {self.food}"
+        return f"{self.capacity} людей предпочитают не есть мясо! " \
+               f"Они предпочитают {self.food}."
 
 
 class MeatEater(Initialization):
@@ -22,7 +22,7 @@ class MeatEater(Initialization):
         super().__init__(capacity, food)
 
     def __str__(self):
-        return f"{self.capacity} мясоедов в Москве! Помимо мясо они едят еще и {self.food}"
+        return f"{self.capacity} мясоедов в Москве! Помимо мясо они едят еще и {self.food}."
 
 
 class SweetTooth(Initialization):
@@ -30,7 +30,7 @@ class SweetTooth(Initialization):
         super().__init__(capacity, food)
 
     def __str__(self):
-        return f"Сладкоежик в Москве {self.capacity}. Их самая любимая еда {self.food}"
+        return f"Сладкоежик в Москве {self.capacity}. Их самая любимая еда {self.food}."
 
     def __eq__(self, other: (int, MeatEater, Vegetarian)):
         if isinstance(other, int):
@@ -38,13 +38,35 @@ class SweetTooth(Initialization):
         elif issubclass(other, Initialization):
             return self.capacity == other.capacity
         else:
-            return f"Невозможно сравнить количество сладкоежек с {other}"
+            return f"Невозможно сравнить количество сладкоежек с {other}."
 
     def __lt__(self, other):
         if isinstance(other, int):
             return self.capacity < other
-        elif issubclass(other, Initialization):
+        elif isinstance(other, (MeatEater, Vegetarian)):
             return self.capacity < other.capacity
         else:
-            return f"Невозможно сравнить количество сладкоежек в {other}"
+            return f"Невозможно сравнить количество сладкоежек в {other}."
 
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.capacity > other
+        elif isinstance(other, (MeatEater, Vegetarian)):
+            return self.capacity > other.capacity
+        else:
+            return f"Невозможно сравнить сладкоежек с {other}."
+
+
+if __name__ == '__main__':
+    v_first = Vegetarian(10_000, ["Орехи", "овощи", "фрукты"])
+    print(v_first)
+    v_second = Vegetarian([23], ["nothing"])
+    m_first = MeatEater(15_000, ["Жареную картошку", "рыба"])
+    print(m_first)
+    s_first = SweetTooth(30_000, ["Мороженное", "Чипсы", "Шоколад"])
+    print(s_first)
+    print(s_first > v_first)
+    print(30000 == s_first)
+    print(s_first == 25000)
+    print(100_000 < s_first)
+    print(100 < s_first)
