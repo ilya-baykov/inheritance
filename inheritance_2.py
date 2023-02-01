03ef44013d72e5a10fd7a5b952e1f151cd4fffad
ilya-baykov <i-baykov@internet.ru>
2023-02-01 14:27:05 +0300

    Добавил класс MeatEater, изменил артрибут food во всех классах на строковое значение

--- inheritance_2.py
+++ inheritance_2.py
@@ -2,7 +2,7 @@ class Initialization:
     def __init__(self, capacity: int, food: list[str]):
         if isinstance(capacity, int):
             self.capacity = capacity
-            self.food = food
+            self.food = ','.join(food)
         else:
             print("Количество людей должно быть целым числом")
 
@@ -12,8 +12,13 @@ class Vegetarian(Initialization):
         super().__init__(capacity, food)
 
     def __str__(self):
-        res_food = ','.join(self.food)
         return f"{self.capacity} людей предпочитают не есть мясо!" \
-               f"Они предпочитают {res_food}"
+               f"Они предпочитают {self.food}"
 
 
+class MeatEater(Initialization):
+    def __init__(self, capacity: int, food: list[str]):
+        super().__init__(capacity, food)
+
+    def __str__(self):
+        return f"{self.capacity} мясоедов в Москве! Помимо мясо они едят еще и {self.food}"
