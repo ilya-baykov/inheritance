f70ea83b0b19651ef28de3f4bee0256defa4f776
ilya-baykov <i-baykov@internet.ru>
2023-02-01 14:15:11 +0300

    Внес небольшие поправки в выводе сообщений в консоль

--- inheritance.py
+++ inheritance.py
@@ -6,7 +6,7 @@ class Transport:
 
     def __str__(self):
         return f"Тип транспорта {self.kind} марки {self.brand} может развить скорость " \
-               f"{self.max_speed}км/ч"
+               f"{self.max_speed} км/ч"
 
 
 class Car(Transport):
@@ -17,14 +17,14 @@ class Car(Transport):
 
     @property
     def gasoline(self):
-        return f"Осталось бензина на {self.__gasoline_residue}км"
+        return f"Осталось бензина на {self.__gasoline_residue} км"
 
     @gasoline.setter
     def gasoline(self, value: int):
         if isinstance(value, int):
-            self.__gasoline_residue = value
-            print(f"Объем топлива увеличен на {value}л и составляет"
-                  f" {self.__gasoline_residue}л")
+            self.__gasoline_residue += value
+            print(f"Объем топлива увеличен на {value} л и составляет"
+                  f" {self.__gasoline_residue} л")
         else:
             print("Ошибка заправки автомобиля")
 
